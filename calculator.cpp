704088b404e21ef8d29117281073b703e947fa90
Akash.Reddy <akashr.reddy@gmail.com>
2019-12-06 18:12:07 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char op;
+	float num1, num2;
+
+	// It allows user to enter operator i.e. +, -, *,
+    cout << "Enter the operator(+,-,*,/): ";
+	cin >> op;
+
+	// It allow user to enter the operands
+    cout << "Enter 2 numbers : ";
+	cin >> num1 >> num2;
+
+
+	switch (op) {
+
+
+		case '+':
+			cout << num1 + num2;
+			break;
+
+		case '-':
+			cout << num1 - num2;
+			break;
+
+		case '*':
+			cout << num1 * num2;
+			break;
+
+		case '/':
+			cout << num1 / num2;
+			break;
+
+
+		default:
+			cout << "Error! operator is not correct";
+			break;
+	}
+
+	return 0;
+}
